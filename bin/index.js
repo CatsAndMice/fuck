ebdc1509b285b1e116975b6f96357b464abb531c
“@lihai” <2322873900@qq.com>
2021-08-29 13:54:16 +0800

    build: 添加提交规范

--- bin/index.js
+++ bin/index.js
@@ -1,22 +1,21 @@
 #!/usr/bin/env node
-const { program } = require('commander'),
-    process = require('process'),
-    package = require('../package.json'),
-    create = require('../src/create'),
-    commands = ["create"]
-program.version(`@lihai-js/cli ${package.version}`, '-v,-version', '获取版本');
+const { program } = require('commander')
+const process = require('process')
+const myPackage = require('../ackage.json')
+const create = require('../src/create')
+program.version(`@lihai-js/cli ${myPackage.version}`, '-v,-version', '获取版本')
 
 program.command('create <project-name>')
-    .description('创建一个文件')
-    .action((name) => {
-        create(name);
-    })
-
+  .description('创建一个文件')
+  .action((name) => {
+    create(name)
+  })
 
 program.on('--help', () => {
-    console.log('\n cli <cammand> -h 查看详情\n');
-});
+  console.log('\n cli <cammand> -h 查看详情\n')
+})
 
-let curCommand = process.argv.slice(3).join('');
-curCommand ? null : program.help();
-program.parse(process.argv);
+const curCommand = process.argv.slice(3).join('')
+!curCommand && program.help()
+
+program.parse(process.argv)
--- src/create.js
+++ src/create.js
@@ -1,7 +1,23 @@
-const axios = require('axios'),
-    fs = require('fs'),
-    path = require('path');
-module.exports = function create(dirName) {
-    const drirPath = path.resolve(__dirname, dirName)
+const fs = require('fs')
+const colors = require('colors')
+const path = require('path')
 
+function createMkdir (drirPath) {
+  try {
+    fs.mkdirSync(drirPath)
+  } catch (e) {
+    console.log(e)
+    console.log(colors.red('error:') + `${drirPath}创建失败，请重新创建!`)
+  }
+}
+
+module.exports = function create (dirName) {
+  const drirPath = path.resolve(process.cwd(), dirName)
+  try {
+    fs.accessSync(drirPath, fs.constants.R_OK)
+    console.log(colors.red('error:') + `${dirName}已存在,请更改文件名!`)
+    process.exit(1)
+  } catch {
+    createMkdir(drirPath)
+  }
 }
